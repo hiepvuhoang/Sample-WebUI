d83f6b89b7dd49568fb1a08e1a1f0cf07487130d
hiepvuhoang <hiep.vu@katalon.com>
2022-11-24 15:02:14 +0700

    áldkajsd

--- Scripts/Main Test Cases/TC3_Visual Testing Example/Script1603792843932.groovy
+++ Scripts/Main Test Cases/TC3_Visual Testing Example/Script1603792843932.groovy
@@ -24,7 +24,7 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
  * https://forum.katalon.com/t/update-with-katalon-studio-7-7-early-release-of-katalon-testops-visual-testing-image-comparison/45557.
  */
 
-WebUI.openBrowser(null)
+
 
 WebUI.comment('Story: Book an appointment')
 
