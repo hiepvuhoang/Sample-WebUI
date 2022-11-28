37a25420e71b5abe56113b6bb3618e9e4d63a7f4
hiepvuhoang <hiep.vu@katalon.com>
2022-11-28 11:43:27 +0700

    add empty test suite

---
+++ Test Suites/empty-testcase.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>empty-testcase</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>366041db-a960-4758-ac3e-e23da35e0be4</testSuiteGuid>
+</TestSuiteEntity>
